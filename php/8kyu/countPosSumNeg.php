aa50f2d30d5d54b598b0fdbc5139723d087c199c
issam-mhj <issammahtaj02@gmail.com>
2025-08-05 15:56:28 +0100

    countP&SumN php

--- php/8kyu/countPosSumNeg.php
+++ php/8kyu/countPosSumNeg.php
@@ -16,6 +16,10 @@ For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should r
 <?php
 function countPositivesSumNegatives($input)
 {
+    $sum = [];
+    if (empty($input)) {
+        return $sum;
+    }
     $sum = array(0, 0);
     for ($i = 0; $i < count($input); $i++) {
         if ($input[$i] > 0) {
